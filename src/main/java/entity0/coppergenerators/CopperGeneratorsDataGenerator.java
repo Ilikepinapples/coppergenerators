3ddeab01176d45ee62c26b8d921b5f6eaa764f12
Ilikepinapples <willschofield08@gmail.com>
2024-08-08 17:05:07 +0100

    Initial commit

---
+++ src/main/java/entity0/coppergenerators/CopperGeneratorsDataGenerator.java
@@ -0,0 +1,11 @@
+package entity0.coppergenerators;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CopperGeneratorsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
