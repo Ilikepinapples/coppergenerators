1472e57406a402197e94122c7c135c1b730bff30
Ilikepinapples <willschofield08@gmail.com>
2024-08-19 22:50:00 +0100

    F9inish adding new content, recipes and other data done, prepared for release

--- src/main/java/entity0/coppergenerators/CopperGeneratorsDataGenerator.java
+++ src/main/java/entity0/coppergenerators/CopperGeneratorsDataGenerator.java
@@ -1,11 +1,33 @@
 package entity0.coppergenerators;
 
+import entity0.coppergenerators.blocks.modBlocks;
+import entity0.coppernetworks.ModBlocks;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
 
 public class CopperGeneratorsDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack CopperGen = fabricDataGenerator.createPack();
+		CopperGen.addProvider(GeneratorBlockLootTables::new);
+	}
 
+	private static class GeneratorBlockLootTables extends FabricBlockLootTableProvider {
+		public GeneratorBlockLootTables(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
+			super(dataOutput, registryLookup);
+		}
+
+		@Override
+		public void generate() {
+			addDrop(modBlocks.COPPERFIEDSCULKCATALYST, modBlocks.COPPERFIEDSCULKCATALYST);
+			addDrop(modBlocks.PASSIVEVOIDGENERATOR, modBlocks.PASSIVEVOIDGENERATOR);
+			addDrop(modBlocks.SOLARCOPPERGENERATOR, modBlocks.SOLARCOPPERGENERATOR);
+			addDrop(modBlocks.THERMOPHOTOVOLTAICGENERATOR, modBlocks.THERMOPHOTOVOLTAICGENERATOR);
+		}
 	}
 }
--- src/main/java/entity0/coppergenerators/blocks/modBlocks.java
+++ src/main/java/entity0/coppergenerators/blocks/modBlocks.java
@@ -1,10 +1,14 @@
 package entity0.coppergenerators.blocks;
 
 import entity0.coppergenerators.CopperGenerators;
+import entity0.coppernetworks.ModBlocks;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.MapColor;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
@@ -26,24 +30,30 @@ public class modBlocks {
     }
     public static void initialize() {
         CopperGenerators.LOGGER.info("initialising blocks");
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.REDSTONE).register((itemGroup) -> {
+            itemGroup.add(modBlocks.SOLARCOPPERGENERATOR.asItem());
+            itemGroup.add(modBlocks.THERMOPHOTOVOLTAICGENERATOR.asItem());
+            itemGroup.add(modBlocks.PASSIVEVOIDGENERATOR.asItem());
+            itemGroup.add(modBlocks.COPPERFIEDSCULKCATALYST.asItem());
+        });
     }
     public static final Block SOLARCOPPERGENERATOR = register(
-            new SolarGenerator(AbstractBlock.Settings.create().sounds(BlockSoundGroup.COPPER)),
+            new SolarGenerator(AbstractBlock.Settings.create().mapColor(MapColor.ORANGE).requiresTool().strength(3.0F, 6.0F).sounds(BlockSoundGroup.COPPER)),
             "solar_generator",
             true
     );
     public static final Block THERMOPHOTOVOLTAICGENERATOR = register(
-            new ThermoPhotoVoltaicGenerator(AbstractBlock.Settings.create().sounds(BlockSoundGroup.COPPER)),
+            new ThermoPhotoVoltaicGenerator(AbstractBlock.Settings.create().mapColor(MapColor.ORANGE).requiresTool().strength(3.0F, 6.0F).sounds(BlockSoundGroup.COPPER)),
             "thermophotovoltaic_generator",
             true
     );
     public static final Block PASSIVEVOIDGENERATOR = register(
-            new PassiveVoidGenerator(AbstractBlock.Settings.create().sounds(BlockSoundGroup.COPPER)),
+            new PassiveVoidGenerator(AbstractBlock.Settings.create().mapColor(MapColor.ORANGE).requiresTool().strength(3.0F, 6.0F).sounds(BlockSoundGroup.COPPER)),
             "passive_void_generator",
             true
     );
     public static final Block COPPERFIEDSCULKCATALYST = register(
-            new CopperfiedSculkCatalyst(AbstractBlock.Settings.create().sounds(BlockSoundGroup.COPPER)),
+            new CopperfiedSculkCatalyst(AbstractBlock.Settings.create().mapColor(MapColor.ORANGE).requiresTool().strength(3.0F, 6.0F).sounds(BlockSoundGroup.COPPER)),
             "copperfied_sculk_catalyst",
             true
     );
